2e6fb837e33b81c0866417d7a8dda74908abddde
Thpavel <tanvirhossenpavel2018@gmail.com>
2025-12-17 14:57:05 +0600

    Fix wsgi.py to use correct settings module

--- tours/tours/wsgi.py
+++ tours/tours/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tours.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tours.tours.settings')
 
 application = get_wsgi_application()
