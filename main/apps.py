a454bbc567200010a9d8ad057042dd1d8e5f1a09
Thpavel <tanvirhossenpavel2018@gmail.com>
2026-05-24 22:45:55 +0530

    Added temporary superuser creation

--- main/apps.py
+++ main/apps.py
@@ -4,3 +4,18 @@ from django.apps import AppConfig
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
+
+    def ready(self):
+        from django.contrib.auth import get_user_model
+
+        User = get_user_model()
+
+        try:
+            if not User.objects.filter(username="admin").exists():
+                User.objects.create_superuser(
+                    username="admin",
+                    email="admin@example.com",
+                    password="admin12345"
+                )
+        except Exception as e:
+            print(e)
